5db97d5a95b33b4ddb5727a0ecf5ac096ec00a7e
Borhan <brborhan70@gmail.com>
2023-06-09 01:53:31 +0600

    navbar and container components is complate

---
+++ src/components/Shared/Container.jsx
@@ -0,0 +1,9 @@
+const Container = ({ children }) => {
+    return (
+      <div className='max-w-[2520px] mx-auto xl:auto md:px-10 sm:px-2 px-4 w-full lg:w-4/5'>
+        {children}
+      </div>
+    )
+  }
+  
+export default Container;
---
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,65 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from '../../../assets/logo/logo.jpg'
+
+
+const Navbar = () => {
+  const NavOptions = (
+    <>
+      <li className="">
+        <NavLink to="/" className='bg-transparent font-semibold hover:scale-125  hover:text-purple-400 hover:font-bold transition text-black'>Home</NavLink>
+      </li>
+      <li>
+        <NavLink className='bg-transparent  font-semibold hover:scale-125  hover:text-purple-500 hover:font-bold transition text-black'>Instructors</NavLink>
+      </li>
+      <li>
+        <NavLink className='bg-transparent font-semibold  hover:scale-125  hover:text-purple-600 hover:font-bold transition text-black' >Classes</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div>
+      <div className="navbar fixed  z-10    bg-white ">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {NavOptions}
+            </ul>
+          </div>
+          <Link to='/'>
+            <div className="ml-8 flex justify-center gap-1 group items-center">
+                <img src={logo} className="w-14 h-12" alt="" />
+                <h1 className=" group-hover:scale-110 text-2xl font-bold italic hidden md:block text-transparent bg-gradient-to-r from-purple-400 to-purple-800 bg-clip-text transition "> Dance Studio</h1>
+            </div>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{NavOptions}</ul>
+        </div>
+        <div className="navbar-end">
+        <NavLink to='/login' className='bg-transparent font-semibold  hover:scale-125  hover:text-purple-600 hover:font-bold transition text-black' >Login</NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
