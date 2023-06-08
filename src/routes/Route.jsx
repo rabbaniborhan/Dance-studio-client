2cde57892c71e79535b59ba27e6b39a9a8657305
Borhan <brborhan70@gmail.com>
2023-06-09 01:46:16 +0600

    router setup complate  and successfull

---
+++ src/routes/Route.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import SingUp from "../Pages/SingUp/SingUp";
+
+ export const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Main></Main>,
+      children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        }
+      ]
+    },
+    {
+        path:'/login',
+        element:<Login></Login>
+    },
+    {
+        path:'/singup',
+        element:<SingUp></SingUp>
+    }
+  ]);
